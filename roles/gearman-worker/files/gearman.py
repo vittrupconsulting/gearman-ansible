ac383bc48da21be3f29ad9971ea29538646b4e14
guest-kevi <guest-kevi@systematic.com>
2022-09-27 14:44:02 +0200

    .

--- roles/gearman-worker/files/gearman.py
+++ roles/gearman-worker/files/gearman.py
@@ -1,7 +1,6 @@
 import sys
 import gear
 import subprocess
-import logging
 from systemd import journal
 import socket
 
@@ -11,17 +10,8 @@ for server in sys.argv[1:]:
     worker.addServer(f"{server}")
 worker.registerFunction("ansible")
 
-# logger = logging.getLogger('custom_logger_name')
-# logger.addHandler(journal.JournalHandler(SYSLOG_IDENTIFIER='custom_unit_name'))
-# logger.warning("1Some message: %s", 'detail')
-# logger.debug("2Some message: %s", 'detail')
-# logger.error("3Some message: %s", 'detail')
-# logger.critical("4Some message: %s", 'detail')
-
 while True:
     job = worker.getJob()
     journal.send(f"Gearman client {job.arguments.decode()}")
-#    subprocess.run(f"whoami", shell=True)
-#    subprocess.run(f"ansible-playbook -i '{job.arguments.decode()},' /etc/ansible/common.yml", shell=True)
     subprocess.run(f"ansible-playbook -i '{job.arguments.decode()},' /etc/ansible/common.yml", shell=True)
     job.sendWorkComplete()
