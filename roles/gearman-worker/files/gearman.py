5b48125d9417a58e8aa48b57cf37a36864da49af
Kenneth Vittrup <kenneth@vittrupconsulting.dk>
2022-10-10 10:15:51 +0200

    writing to etcd on callback

--- roles/gearman-worker/files/gearman.py
+++ roles/gearman-worker/files/gearman.py
@@ -1,4 +1,5 @@
 import gear
+from etcd3 import Client
 import subprocess
 from systemd import journal
 import json
@@ -13,8 +14,11 @@ for server in servers:
     worker.addServer(f"{server}")
 worker.registerFunction("common")
 
+client = Client('127.0.0.1', 2379)
+
 while True:
     job = worker.getJob()
     journal.send(f"Gearman client {job.arguments.decode()}")
+    client.put(f"{job.arguments.decode()}", f"{job.arguments.decode()}")
     subprocess.run(f"ansible-playbook --inventory '{job.arguments.decode()},' /etc/ansible/common.yml", shell=True)
     job.sendWorkComplete()
