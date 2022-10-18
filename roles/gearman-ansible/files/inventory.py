3009fb5ab1f3270894d9496083af01fee6091b82
Kenneth Vittrup <kenneth@vittrupconsulting.dk>
2022-10-18 21:56:47 +0200

    inventory update

--- roles/gearman-ansible/files/inventory.py
+++ roles/gearman-ansible/files/inventory.py
@@ -3,18 +3,19 @@
 import etcd3
 import json
 
-etcd = etcd3.client(host='192.168.0.154', port=2379)
+etcd = etcd3.client(host='192.168.147.54', port=2379)
 
 inventory = {}
 inventory["_meta"] = {}
-inventory["_meta"]["hostvars"] = {}
+#inventory["_meta"]["hostvars"] = {}
 
 for (_, m) in etcd.get_all():
     for role in etcd.get(m.key)[0].decode().split(','):
-        hostname = m.key.decode()
-        if role not in inventory.keys():
-            inventory[role] = {}
-            inventory[role]['hosts'] = []
-        inventory[role]['hosts'].append(hostname)
+        if role:
+            hostname = m.key.decode()
+            if role not in inventory.keys():
+                inventory[role] = {}
+                inventory[role]['hosts'] = []
+            inventory[role]['hosts'].append(hostname)
 
 print(json.dumps(inventory))
